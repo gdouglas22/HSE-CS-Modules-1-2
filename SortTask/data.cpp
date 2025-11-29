cdc99e77647169e589ac79dfe76e4fcc502f01ef
Bogdan Frolov / gdouglas <frolovb880@gmail.com>
2025-11-30 01:36:06 +0300

    Add [data arrays with indx] data.cpp

---
+++ SortTask/data.cpp
@@ -0,0 +1,10 @@
+#include "lesson.h"
+
+Lesson Lessons[MAX_LESSONS];
+int LessonCount = 0;
+
+TitleIndex TitleIdx[MAX_LESSONS];
+int TitleIdxCount = 0;
+
+DateTimeIndex DateIdx[MAX_LESSONS];
+int DateIdxCount = 0;
